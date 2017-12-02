f3d4786884480d395c7d389103e7e1df05dc6586
Teagen Kiel <teagen-kiel@uiowa.edu>
2017-12-02 15:30:15 -0600

    Working on Server

--- StudyGroupMessenger/src/Client.java
+++ StudyGroupMessenger/src/Client.java
@@ -1,10 +1,57 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import java.io.ObjectOutputStream;
+
+import java.net.InetAddress;
+
+import java.net.Socket;
+
 /**
- *
- *
  *
  */
 public class Client {
+    private Socket socket;
+
+    private ObjectOutputStream output;
+
+    private ObjectInputStream input;
+
+    private String message;
+
+    Client(String host, int port) throws IOException {
+
+        socket = new Socket(InetAddress.getByName(host), port);
+
+        output = new ObjectOutputStream(socket.getOutputStream());
+
+        output.flush();
+
+        input = new ObjectInputStream(socket.getInputStream());
+
+        processConnection();
+
+    }
 
 
+    private void processConnection() throws IOException {
+
+        while (message.equals("end")) {
+
+            try {
+
+                message = (String) input.readObject();
+
+                System.out.println(message);
+
+            } catch (ClassNotFoundException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+    }
 
 }
