d40899b2366649c1bdab00301d441d3da05875db
Aarron Lebow <aarron-lebow@uiowa.edu>
2017-12-03 13:41:59 -0600

    updated multithreaded server

--- StudyGroupMessenger/src/WrkrRunnable.java
+++ StudyGroupMessenger/src/WrkrRunnable.java
@@ -1,11 +1,11 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class WrkrRunnable implements Runnable {
     private Socket client = null;
     private String message = null;
+    private ObjectInputStream input;
+    private ObjectInputStream output;
 
     WrkrRunnable(Socket client, String message) {
         this.client = client;
@@ -14,13 +14,20 @@ public class WrkrRunnable implements Runnable {
 
     public void run(){
         try{
-            InputStream input = client.getInputStream();
-            OutputStream output = client.getOutputStream();
-            System.out.println("Thread");
+            ObjectInputStream input = new ObjectInputStream(client.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(client.getOutputStream());
+            try {
+                message = (String) input.readObject();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            System.out.println(message);
             output.close();
             input.close();
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+
+
 }
