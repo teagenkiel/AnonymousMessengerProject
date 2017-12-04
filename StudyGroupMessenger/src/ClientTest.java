ba03f1d45560eef9503797cd16d355fd82a2456e
Joe Biggins <joseph-biggins@uiowa.edu>
2017-12-03 18:42:02 -0600

    try

--- StudyGroupMessenger/src/ClientTest.java
+++ StudyGroupMessenger/src/ClientTest.java
@@ -1,3 +1,9 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
 /**
@@ -5,7 +11,14 @@ import java.io.IOException;
  *
  *
  */
-public class ClientTest {
+public class ClientTest extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
     public static void main(String args[]){
         Client myClient;
@@ -17,6 +30,5 @@ public class ClientTest {
             e.printStackTrace();
         }
     }
-
 
 }
