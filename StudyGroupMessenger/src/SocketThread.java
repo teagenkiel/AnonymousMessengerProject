82232b3b5fec1e76da4f2a259d116f6f774542a0
Aarron Lebow <aarron-lebow@uiowa.edu>
2017-12-03 22:06:18 -0600

    server handles multiple clients

--- StudyGroupMessenger/src/SocketThread.java
+++ StudyGroupMessenger/src/SocketThread.java
@@ -7,38 +7,33 @@ import java.net.Socket;
 public class SocketThread implements Runnable {
     private ObjectInputStream input;
     private ObjectOutputStream output;
-    private ServerSocket server;
     private Socket socket;
+    private String message = "";
 
     SocketThread(Socket socket){
         this.socket = socket;
     }
 
-    public void run(){
-       // while (true) {
-            System.out.println("thread is running");
-            try {
-                input = new ObjectInputStream(socket.getInputStream());
-                output = new ObjectOutputStream(socket.getOutputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String message = null;
-            try {
-                message = (String) input.readObject();
-                System.out.println(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
+    public void run() {
+        // while (true) {
+        System.out.println("thread is running");
+        try {
+            input = new ObjectInputStream(socket.getInputStream());
+            output = new ObjectOutputStream(socket.getOutputStream());
+            processConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("got IO streams");
+
+        System.out.println("read messages");
             try {
                 output.writeObject("message from Server");
                 output.flush();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.println(message);
+            System.out.println("wrote messages");
             try {
                 input.close();
                 output.close();
@@ -46,7 +41,23 @@ public class SocketThread implements Runnable {
                 e.printStackTrace();
             }
             System.out.println("connection closed");
+
       //  }
+
+    }
+
+    private void processConnection()throws IOException{
+        System.out.println("program entered processConnection");
+       // while(!message.equals("end")) {
+            try {
+                message = (String) input.readObject();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            System.out.println("message received");
+            System.out.println(message);
+       // }
+        System.out.print("program finished processConnection");
 
     }
 }
