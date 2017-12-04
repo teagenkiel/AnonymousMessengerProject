8079d5b79b2762e1e2698ccdffc9500b0be1fffc
Teagen Kiel <teagen-kiel@uiowa.edu>
2017-12-04 02:44:43 -0600

    Created BoardModel which will communicate with all controllers

---
+++ StudyGroupMessenger/src/ClientModel.java
@@ -0,0 +1,29 @@
+/**
+ * Holds the client for each instance of a StudyGroup app so it can be used by each scene's controller efficiently.
+ *
+ *
+ */
+public class ClientModel {
+
+
+    private Client client;
+    private String clientHostAddress;
+    private int clientPortNumber;
+
+    public ClientModel(String clientHostAddress, int clientPortNumber){
+
+        this.client = new Client(clientHostAddress, clientPortNumber);
+        this.clientHostAddress = clientHostAddress;
+        this.clientPortNumber = clientPortNumber;
+        //swag me 1738
+
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
