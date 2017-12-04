7bde7b184fd39331051a98247172ee97acffc66c
Joe Biggins <joseph-biggins@uiowa.edu>
2017-12-03 18:28:19 -0600

    "I need to"

--- StudyGroupMessenger/src/Client.java
+++ StudyGroupMessenger/src/Client.java
@@ -4,15 +4,19 @@ import javax.swing.plaf.basic.BasicOptionPaneUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.io.ObjectInputStream;
-
 import java.io.ObjectOutputStream;
-
 import java.net.InetAddress;
-
 import java.net.Socket;
 import java.util.EventListener;
+import java.io.EOFException;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 
 /**
  *
@@ -25,26 +29,42 @@ public class Client {
     private String host;
     private int port;
 
+    @FXML
+    TextField messageBox;
+
+    @FXML
+    Button send;
+
+    @FXML
+    TextArea messageDisplay;
+
 
     Client(String host, int port) throws IOException {
+
         this.host = host;
         this.port = port;
 
     }
 
-    public void runClient() throws IOException {
+    public void runClient() throws IOException{
         socket = new Socket(InetAddress.getByName(host), port);
         output = new ObjectOutputStream(socket.getOutputStream());
         output.flush();
         input = new ObjectInputStream(socket.getInputStream());
-        output.writeObject("message from client");
-        output.flush();
         processConnection();
+    }
+
+    public void processSend(ActionEvent event){
+
+        if(!messageBox.getText().equals("")){
+            display(messageBox.getText());
+        }
     }
 
 
     private void processConnection() throws IOException {
         message = "";
+
         while (!message.equals("end")) {
             try {
                 message = (String) input.readObject();
@@ -55,11 +75,17 @@ public class Client {
         }
     }
 
-    //private void display(String message){
+    private void display(String message){
+        new Runnable(){
 
-    //}
+            @Override
+            public void run() {
+                messageDisplay.setText(messageDisplay.getText() + message);
+            }
+        };
+    }
 
-    private void send(String message) {
+    private void send(String message){
         try {
             output.writeObject(message);
             output.flush();
